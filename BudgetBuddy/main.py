76eb2ad8198c0edba02f5f1b75c764abb68fd323
iNotEsco <47795966+iNotEsco@users.noreply.github.com>
2020-04-22 18:22:03 -0400

    update

--- BudgetBuddy/main.py
+++ BudgetBuddy/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify, json
-import _sqlite3
+from flask import Flask, render_template, jsonify, json, request, redirect, url_for
+import _sqlite3 as sql
+import LogIn as dbHandler
 import util
-
 
 app = Flask(__name__)
 
@@ -10,41 +10,50 @@ app = Flask(__name__)
 def index():
     # this is your index page
     log = 'Index.'
-    return render_template('Account.html', log_index = log)
+    return render_template('Account.html', log=log)
 
-@app.route('/LogIn')
-def background():
-    # this is your LogIn page
-    log = 'LogIn.'
-    return render_template('LogIn.html', log_background = log)\
+
+@app.route('/LogIn', methods=['POST'], ['GET'])
+def log_in():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        dbHandler.insertUser(username, password)
+        users = dbHandler.retrieveUsers()
+        return render_template('LogIn.html', users=users)
+    else:
+        return render_template('index.html')
+
 
 @app.route('/Registration')
-def background():
+def registration():
     # this is your LogIn page
-    log = 'LogIn.'
-    return render_template('Registration.html', log_background = log)
+    log = 'Registration.'
+    return render_template('Registration.html', log=log)
+
 
 @app.route('/Forecast')
-def background():
+def forecast():
     # this is your LogIn page
-    log = 'LogIn.'
-    return render_template('Forecast.html', log_background = log)
+    log = 'Forecast.'
+    return render_template('Forecast.html', log=log)
+
 
 @app.route('/MonthlyExpenses')
-def background():
+def monthly_expenses():
     # this is your LogIn page
-    log = 'LogIn.'
-    return render_template('MonthlyExpenses.html', log_background = log)
+    log = 'MonthlyExpenses.'
+    return render_template('MonthlyExpenses.html', log=log)
+
 
 @app.route('/Income')
-def background():
+def income():
     # this is your LogIn page
-    log = 'LogIn.'
-    return render_template('Income.html', log_background = log)
+    log = 'Income.'
+    return render_template('Income.html', log=log)
 
 
 if __name__ == '__main__':
     app.debug = True
     ip = '127.0.0.1'
     app.run(host=ip)
-
