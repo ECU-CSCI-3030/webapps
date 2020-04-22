ffd9c221580cdafdb8ae8552e1ea1bff1b8a8983
iNotEsco <47795966+iNotEsco@users.noreply.github.com>
2020-04-22 18:34:31 -0400

    update

--- BudgetBuddy/main.py
+++ BudgetBuddy/main.py
@@ -1,56 +1,76 @@
 from flask import Flask, render_template, jsonify, json, request, redirect, url_for
 import _sqlite3 as sql
-import LogIn as dbHandler
-import util
+import hashlib
 
 app = Flask(__name__)
+
+
+def validate(username, password):
+    con = sql.connect('static/user.db')
+    completion = False
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Users")
+        rows = cur.fetchall()
+        for row in rows:
+            dbUser = row[0]
+            dbPass = row[1]
+            if dbUser == username:
+                completion = check_password(dbPass, password)
+    return completion
+
+
+def check_password(hashed_password, user_password):
+    return hashed_password == hashlib.md5(user_password.encode()).hexdigest()
 
 
 @app.route('/')
 def index():
     # this is your index page
     log = 'Index.'
-    return render_template('Account.html', log=log)
+    return render_template('account.html', log=log)
 
 
-@app.route('/LogIn', methods=['POST'], ['GET'])
+@app.route('/log_in', methods=['POST'], ['GET'])
 def log_in():
-    if request.method=='POST':
+    error = None
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        dbHandler.insertUser(username, password)
-        users = dbHandler.retrieveUsers()
-        return render_template('LogIn.html', users=users)
-    else:
-        return render_template('index.html')
+        completion = validate(username, password)
+        if completion == False:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('account'))
+    return render_template('login.html', error=error)
 
 
-@app.route('/Registration')
+@app.route('/registration')
 def registration():
     # this is your LogIn page
     log = 'Registration.'
-    return render_template('Registration.html', log=log)
+    return render_template('registration.html', log=log)
 
 
-@app.route('/Forecast')
+@app.route('/forecast')
 def forecast():
     # this is your LogIn page
     log = 'Forecast.'
-    return render_template('Forecast.html', log=log)
+    return render_template('forecast.html', log=log)
 
 
-@app.route('/MonthlyExpenses')
+@app.route('/monthly_expenses')
 def monthly_expenses():
     # this is your LogIn page
     log = 'MonthlyExpenses.'
-    return render_template('MonthlyExpenses.html', log=log)
+    return render_template('monthly_expenses.html', log=log)
 
 
-@app.route('/Income')
+@app.route('/income')
 def income():
     # this is your LogIn page
     log = 'Income.'
-    return render_template('Income.html', log=log)
+    return render_template('income.html', log=log)
 
 
 if __name__ == '__main__':
